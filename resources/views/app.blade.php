355e4b75e23bbaad8364c7d86050519dd7fd3da0
forkanimahdi <forkanimahdi@gmail.com>
2025-03-08 13:14:22 +0000

    fix local csrf

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        @if (app()->environment('production'))
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        @endif
+    
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
             (function() {
