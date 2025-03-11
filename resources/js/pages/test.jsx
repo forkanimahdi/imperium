9bf0ee1da610e9e10dce81b93f03b168667c736e
forkanimahdi <forkanimahdi@gmail.com>
2025-03-11 18:48:26 +0000

    imperium structure

---
+++ resources/js/pages/test.jsx
@@ -0,0 +1,10 @@
+
+export default function Rere() {
+
+    return (
+       
+       <div className="flex items-center justify-center h-screen">
+        <h1 className="text-white"> hello ddd</h1>
+       </div>
+    );
+}
